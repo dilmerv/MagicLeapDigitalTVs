091da315bdd4f7120cb6550567ef9f71f78363ed
Dilmer Valecillos <dilmervst@gmail.com>
2019-11-11 07:02:47 -0700

    Initial project files which includes first demo

---
+++ Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(RawImage))]
+public class StreamVideo : MonoBehaviour
+{
+    [SerializeField]
+    private float waitToPlayForSeconds = 1.0f;
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private RawImage rawImage;
+
+    private void Start() => StartVideo();
+
+    public void StartVideo() => StartCoroutine(PlayVideo());
+
+    public void StopVideo() => Debug.Log("Not Implemented");
+
+    private IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(waitToPlayForSeconds);
+
+        while(!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+    }
+}
